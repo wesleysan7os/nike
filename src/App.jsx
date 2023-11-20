3f9346489fab443d4ad495e399dd9a19523b659f
wesleysan7os <wesley.santos.comp@gmail.com>
2023-11-20 13:47:46 -0300

    feat: popular product card component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import './index.css'
 const App = () => (
   <main className='relative'>
     <Nav />
-    <section className='xl:padding-1 wide:padding-l padding-b'>
+    <section className='xl:padding-l wide:padding-r padding-b'>
       <Hero />
     </section>
     <section className='padding'>
